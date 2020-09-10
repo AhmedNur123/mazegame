6d448b8984fe9e1adf850bbb31bab15c1883b0c1
Sudheshwer Rai <sudheshwer.tech@gmail.com>
2020-09-10 20:48:23 +0530

    Add project files.

---
+++ WindowsApplicationWithAhmad/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace WindowsApplicationWithAhmad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }       
+        private void pictureBox3_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You hit the ball game is over","Sorry");
+            
+        }       
+
+        private void pictureBox6_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You won the game.", "Congratulations");
+        }
+        
+    }
+}
